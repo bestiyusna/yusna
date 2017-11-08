24ad0cf6487b9bd3208d748d0a4498e610ab8ac6
bestiyusna <33086600+bestiyusna@users.noreply.github.com>
2017-11-08 04:39:11 -0800

    membuat dan menampilkan menetukan nilai Positif, negattif dan nol

--- No 4.cpp
+++ No 4.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 int main(){
+	int x;
+	cout<<"Masukkan Nilai :";
+	cin>>x;
 	
+	if(x>0){
+		cout<<"Nilai Positif";
+	}
+	else if (x<0){
+		cout<<"Nilai Negatif";
+		
+	}
+	
+	else {
+		cout<<"Nol";
+	}
 }
