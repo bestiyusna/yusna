28682c0ffb096f5750a3e16bf9481cdd015578d8
bestiyusna <33086600+bestiyusna@users.noreply.github.com>
2017-11-08 04:30:41 -0800

    menghitung dan menampilkan penghitungan gaji karyawan

--- No 3.cpp
+++ No 3.cpp
@@ -6,4 +6,21 @@ int main (){
 	int gp,jk;
 	int lembur,gs,pjk,gk;
 	
+	cout <<"Penghitungan gaji Karyawan"<<endl;
+	cout<<"Masukkan Gaji Pokok Anda : Rp.";
+	cin>>gp;
+	cout<<"Masukkan Jam Kerja Anda : ";
+	cin>>jk;
+	
+	lembur=jk-40;
+	gs=(lembur*1.5+40)*gp;
+	pjk=gs*0.15;
+	gk=gs-pjk;
+	
+	cout<<endl;
+	cout<<"Gaji Pokok 	: Rp."<<gp<<endl;
+	cout<<"Jam Kerja  	: Rp."<<jk<<endl;
+	cout<<"Lembur		: Rp."<<lembur<<endl;
+	cout<<"Gaji Sebelum Dipotong Pajak  : Rp."<<gs<<endl;
+	cout<<"Total Gaji Anda Adalah		: Rp."<<gk<<endl;
 }
