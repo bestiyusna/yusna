b306d3849235a458fb8ead30d782ca56d80a48d5
bestiyusna <33086600+bestiyusna@users.noreply.github.com>
2017-11-08 03:18:18 -0800

    menghitung selisih

--- No1.cpp
+++ No1.cpp
@@ -5,4 +5,17 @@ using namespace std;
 int main (){
 	int x,y,z;
 	
+	cout<<"menghitung selisih absoulute"<<endl;
+	cout<<"masukan nilai x :";
+	cin>>x;
+	cout<<"masukan nilai y :";
+	cin>>y;
+	
+	if (x>y){
+		z=x-y;
+	}
+	else if(y>x){
+		z=y-x;
+	}
+	cout<<"hasil :"<<z;
 }
